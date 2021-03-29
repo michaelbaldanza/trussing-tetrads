f7d34e7476ef6929deb937927ec4b0812776bf3e
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-03-29 16:31:26 -0400

    add strikethrough effect for vertcal and diagonal victories

--- js/main.js
+++ js/main.js
@@ -25,7 +25,8 @@ function init() {
   turn = -1;
   victory = {
     winner: null,
-    vicInds: []
+    indexes: [],
+    type: '',
   };
   render();
 }
@@ -63,15 +64,15 @@ function selectSocket(evt) {
 function render() {
   for (r = 0; r < sockets.length; r++) {
     let strike = socketEls[r].firstChild;
-    if (strike.hasAttribute('class')) strike.removeAttribute('class');
+    if (strike.hasAttribute('strike')) strike.removeAttribute('strike');
     // if (sockets[r] === -1) socketEls[r].style.backgroundColor = '#d8e2dc';
     // if (sockets[r] === 1) socketEls[r].style.backgroundColor = '#fec89a';
     if (sockets[r] === -1) socketEls[r].style.backgroundColor = '#caffbf';
     if (sockets[r] === 1) socketEls[r].style.backgroundColor = '#9bf6ff';
     if (!sockets[r]) socketEls[r].style.backgroundColor = '';
-    if (victory.vicInds.indexOf(r) !== -1) {
+    if (victory.indexes.indexOf(r) !== -1) {
       // socketEls[r].style.backgroundColor = 'maroon';
-      strike.setAttribute('class', 'strike');
+      strike.setAttribute('strike', victory.type);
     }
   }
 }
@@ -86,45 +87,52 @@ function erectColumns() {
   });
 }
 
-function getWinner() {
+function getWinner() {;
+  let type = ''
   let bound = sockets.length - 7;
   for (i = sockets.length - 1; i >= 0; i--) {
     if (sockets[i]) {
       // check winner horizontally
       if (i > bound + 2) {
         const horizontal = [3, 2, 1];
-        checkTetrad(i, horizontal);
+        type = 'horizontal';
+        checkTetrad(i, horizontal, type);
       }
       // check winner vertically
       const vertical = [21, 14, 7];
-      checkTetrad(i, vertical);
+      type = 'vertical';
+      checkTetrad(i, vertical, type);
       // check winner on left diagonal
       const leftDiagonal = [24, 16, 8];
-      checkTetrad(i, leftDiagonal);
+      type = 'left-diagonal'
+      checkTetrad(i, leftDiagonal, type);
       // check winner on right diagonal
       const rightDiagonal = [18, 12, 6];
-      checkTetrad(i, rightDiagonal);
+      type = 'right-diagonal';
+      checkTetrad(i, rightDiagonal, type);
     }
     if (bound === i) bound -= 7;
   }
 }
 
-function checkTetrad(idx, tetrad) {
-  if (victory.vicInds.length === 4) return;
+function checkTetrad(idx, tetrad, type) {
+  if (victory.indexes.length === 4) return;
   let counter = 0;
   for (q = 0; q < tetrad.length; q++) {
     if (sockets[idx] === sockets[idx - tetrad[q]]) {
       let pcIdx = idx - tetrad[q];
-      victory.vicInds.push(pcIdx);
+      victory.indexes.push(pcIdx);
       counter++;
       if (counter === 3) {
-        victory.vicInds.push(idx);
+        victory.indexes.push(idx);
         victory.winner = turn;
+        victory.type = type;
+        console.log(victory.type);
         return;
       }
     }
   }
-  victory.vicInds = [];
+  victory.indexes = [];
 }
 
 function reset() {
